b0cb54233e398e2f0d8b2b3cb082bbd52a5e6369
ulfa hasanah <ulfa2912@outlook.com>
2023-02-13 22:17:59 +0700

    feat(category): add api read category

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
-use App\Models\CategoryProduct;
-use App\Models\ImageProduct;
 
 class ProductController extends Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use App\Http\Controllers\ProductController;
 */
 
 Route::resource('products', ProductController::class);
+Route::resource('categories', CategoryController::class);
