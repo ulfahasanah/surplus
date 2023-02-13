564595b4086592948a5cb2e3c3f6ef552337457d
ulfa hasanah <ulfa2912@outlook.com>
2023-02-13 22:58:50 +0700

    feat(product-image): add api create product image

--- app/Http/Controllers/ProductImageController.php
+++ app/Http/Controllers/ProductImageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ProductImage;
+use App\Http\Requests\ProductImageRequest;
 
 class ProductImageController extends Controller
 {
@@ -21,56 +22,24 @@ class ProductImageController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductImageRequest $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        $productImage = ProductImage::create($request->all());
+        return response()->json([
+            "status" => "success",
+            "data" => $productImage,
+            "message" => "Product Image created successfully"
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ProductImageRequest $request,  ProductImage $productImage)
     {
         //
     }
---
+++ app/Http/Requests/ProductImageRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductImageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'product_id' => [
+                'required', 
+                Rule::exists('products', 'id'),
+                Rule::unique('image_products')->where('image_id', $this->image_id)
+                    ->where('product_id', $this->product_id)
+            ],
+            'image_id' => [
+                'required', 
+                Rule::exists('images', 'id'),
+                Rule::unique('image_products')->where('image_id', $this->image_id)
+                    ->where('product_id', $this->product_id)
+            ]
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => 'error',
+            'message' => $validator->errors()->first()
+        ], 422));
+    }
+}
